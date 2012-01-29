5a949ffa5ebc6cd737bd971bf4798c7ff662fb36
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2012-01-29 21:32:04 +0000

    New data handler

--- json.c
+++ json.c
@@ -1,301 +1,31 @@
 #include "includes.h"
 
-void free_tweet (Tweet_p twt) {
-    size_t i = 0;
-
-    if (twt) {
-        if (twt->retweeted_status)
-            free_tweet (twt->retweeted_status);
-        if (twt->user) {
-            if (twt->user->id_str)
-                free (twt->user->id_str);
-            if (twt->user->description)
-                free (twt->user->description);
-            if (twt->user->profile_image_url)
-                free (twt->user->profile_image_url);
-            if (twt->user->name)
-                free (twt->user->name);
-            if (twt->user->screen_name)
-                free (twt->user->screen_name);
-            free (twt->user);
-        }
-        if (twt->entities) {
-            if (twt->entities->urls) {
-                for (i = 0; i < twt->entities->url_len; i++) {
-                    if (twt->entities->urls[i].url)
-                        free(twt->entities->urls[i].url);
-                    if (twt->entities->urls[i].expanded_url)
-                        free(twt->entities->urls[i].expanded_url);
-                    if (twt->entities->urls[i].display_url)
-                        free(twt->entities->urls[i].display_url);
-                }
-
-                free(twt->entities->urls);
-            }
-
-            if (twt->entities->media) {
-                for (i = 0; i < twt->entities->media_len; i++) {
-                    if (twt->entities->media[i].url)
-                        free(twt->entities->media[i].url);
-                    if (twt->entities->media[i].expanded_url)
-                        free(twt->entities->media[i].expanded_url);
-                    if (twt->entities->media[i].display_url)
-                        free(twt->entities->media[i].display_url);
-                    if (twt->entities->media[i].media_url)
-                        free(twt->entities->media[i].media_url);
-                    if (twt->entities->media[i].type)
-                        free(twt->entities->media[i].type);
-                }
-
-                free(twt->entities->media);
-            }
-
-            free (twt->entities);
-        }
-        if (twt->id_str)
-            free (twt->id_str);
-        if (twt->created_at)
-            free (twt->created_at);
-        if (twt->text)
-            free (twt->text);
-        if (twt->error)
-            free (twt->error);
-        free (twt);
-    }
+enum tweet_element tokenize (char *key) {
+    if (!strncmp(key, "place", strlen(key))) { return TWTELM_PLACE; }
+    if (!strncmp(key, "geo", strlen(key))) { return TWTELM_GEO; }
+    if (!strncmp(key, "retweeted", strlen(key))) { return TWTELM_RETWEETED; }
+    if (!strncmp(key, "text", strlen(key))) { return TWTELM_TEXT; }
+    if (!strncmp(key, "in_reply_to_status_id_str", strlen(key))) { return TWTELM_IN_REPLY_TO_STATUS_ID_STR; }
+    if (!strncmp(key, "in_reply_to_status_id", strlen(key))) { return TWTELM_IN_REPLY_TO_STATUS_ID; }
+    if (!strncmp(key, "in_reply_to_user_id_str", strlen(key))) { return TWTELM_IN_REPLY_TO_USER_ID_STR; }
+    if (!strncmp(key, "in_reply_to_user_id", strlen(key))) { return TWTELM_IN_REPLY_TO_USER_ID; }
+    if (!strncmp(key, "in_reply_to_user_id", strlen(key))) { return TWTELM_IN_REPLY_TO_USER_ID; }
+    if (!strncmp(key, "truncated", strlen(key))) { return TWTELM_TRUNCATED; }
+    if (!strncmp(key, "possibly_sensitive_editable", strlen(key))) { return TWTELM_POSSIBLY_SENSITIVE_EDITABLE; }
+    if (!strncmp(key, "source", strlen(key))) { return TWTELM_SOURCE; }
+    if (!strncmp(key, "contributors", strlen(key))) { return TWTELM_CONTRIBUTORS; }
+    if (!strncmp(key, "retweet_count", strlen(key))) { return TWTELM_RETWEET_COUNT; }
+    if (!strncmp(key, "favorited", strlen(key))) { return TWTELM_FAVORITED; }
+    if (!strncmp(key, "created_at", strlen(key))) { return TWTELM_CREATED_AT; }
+    if (!strncmp(key, "coordinates", strlen(key))) { return TWTELM_COORDINATES; }
+    if (!strncmp(key, "possibly_sensitive", strlen(key))) { return TWTELM_POSSIBLY_SENSITIVE; }
+    if (!strncmp(key, "id_str", strlen(key))) { return TWTELM_ID_STR; }
+    if (!strncmp(key, "user", strlen(key))) { return TWTELM_USER; }
+    if (!strncmp(key, "in_reply_to_screen_name", strlen(key))) { return TWTELM_IN_REPLY_TO_SCREEN_NAME; }
+    if (!strncmp(key, "id", strlen(key))) { return TWTELM_ID; }
+    if (!strncmp(key, "entities", strlen(key))) { return TWTELM_ENTITIES; }
+    return TWTELM_UNDEF;
 }
-
-int32_t int_from_string_object (json_object *object, const char *key) {
-    json_object *sub_object = NULL;
-    char *sub_object_str = NULL;
-    int32_t rc = -1;
-
-    if (!(sub_object = json_object_object_get (object, key))) {
-        goto over;
-    }
-
-    if (!(sub_object_str = (char *) json_object_get_string (sub_object))) {
-        goto over;
-    }
-
-    if (strncmp (sub_object_str, "100+", 4)) {
-        rc = atoi (sub_object_str);
-    } else {
-        rc = 101;
-    }
-over:
-    return rc;
-}
-
-char *string_from_object (json_object *object, const char *key) {
-    json_object *sub_object = NULL;
-    const char *sub_object_str = NULL;
-    char *return_str = NULL;
-
-    if (!(sub_object = json_object_object_get (object, key))) {
-        goto over;
-    }
-
-    if (!(sub_object_str = json_object_get_string (sub_object))) {
-        goto over;
-    }
-
-    if (!(return_str = strdup (sub_object_str))) {
-        goto over;
-    }
-over:
-    return return_str;
-}
-
-/*Tweet_p tweet_create_from_object (json_object *tweet) {
-    Tweet_p twt = NULL, _twt = NULL;
-    json_object *tweet_retweeted_status = NULL,
-                *tweet_user = NULL,
-                *tweet_entities = NULL,
-                *tweet_urls = NULL,
-                *tweet_url = NULL,
-                *tweet_medias = NULL,
-                *tweet_media = NULL;
-    size_t i = 0;
-
-    if (!(_twt = (Tweet_p) malloc (sizeof (struct Tweet)))) {
-        syslog (P_ERR, "Cannot allocate memory for tweet structure");
-        goto over;
-    }
-
-    _twt->id_str = NULL;
-    _twt->created_at = NULL;
-    _twt->text = NULL;
-    _twt->user = NULL;
-    _twt->entities = NULL;
-    _twt->retweeted_status = NULL;
-    _twt->retweet_count = 0;
-    _twt->error = NULL;
-
-    if (!(_twt->user = malloc (sizeof (struct User)))) {
-        syslog (P_ERR, "Cannot allocate memory for tweet user\n");
-        goto over;
-    }
-
-    _twt->user->id_str = NULL;
-    _twt->user->screen_name = NULL;
-    _twt->user->name = NULL;
-    _twt->user->profile_image_url = NULL;
-    _twt->user->description = NULL;
-
-    if (!(_twt->entities = malloc (sizeof (struct Entities)))) {
-        syslog (P_ERR, "Cannot allocate memory for tweet entities\n");
-        goto over;
-    }
-
-    _twt->entities->urls = NULL;
-    _twt->entities->media = NULL;
-    _twt->entities->url_len = 0;
-    _twt->entities->media_len = 0;
-
-    if (json_object_object_get (tweet, "friends") == NULL) {
-        if ((tweet_retweeted_status = json_object_object_get (tweet, "retweeted_status"))) {
-            if (!(_twt->retweeted_status = tweet_create_from_object (tweet_retweeted_status))) {
-                syslog (P_ERR, "Cannot get retweeted status\n");
-                goto over;
-            }
-        }
-
-        if (!(_twt->text = string_from_object (tweet, "text"))) {
-            goto over;
-        }
-
-        if (!(_twt->id_str = string_from_object (tweet, "id_str"))) {
-            syslog (P_ERR, "Cannot retrieve id string");
-            goto over;
-        }
-
-        if (!(_twt->created_at = string_from_object (tweet, "created_at"))) {
-            syslog (P_ERR, "Cannot retrieve created_at string");
-            goto over;
-        }
-
-        if ((_twt->retweet_count = int_from_string_object (tweet, "retweet_count")) == -1) {
-            syslog (P_ERR, "Cannot retrieve retweet_count int");
-        }
-
-        if (!(tweet_user = json_object_object_get (tweet, "user"))) {
-            syslog (P_ERR, "Cannot retrieve user object");
-            goto over;
-        }
-
-        if (!(_twt->user->id_str = string_from_object (tweet_user, "id_str"))) {
-            syslog (P_ERR, "Cannot retrieve user id_str string");
-            goto over;
-        }
-
-        if (!(_twt->user->screen_name = string_from_object (tweet_user, "screen_name"))) {
-            syslog (P_ERR, "Cannot retrieve user screen_name string");
-            goto over;
-        }
-
-        if (!(_twt->user->name = string_from_object (tweet_user, "name"))) {
-            syslog (P_ERR, "Cannot retrieve user name string");
-            goto over;
-        }
-
-        _twt->user->description = string_from_object (tweet_user, "description");
-
-        if (!(_twt->user->profile_image_url = string_from_object (tweet_user, "profile_image_url"))) {
-            syslog (P_ERR, "Cannot retrieve user profile_image_url string");
-            goto over;
-        }
-
-        if (!(tweet_entities = json_object_object_get (tweet, "entities"))) {
-            syslog (P_ERR, "Cannot retrieve entities object");
-            goto over;
-        } else {
-            if ((tweet_urls = json_object_object_get (tweet_entities, "urls"))) {
-                _twt->entities->url_len = json_object_array_length(tweet_urls);
-
-                if (!(_twt->entities->urls = malloc (sizeof(struct Url) * _twt->entities->url_len))) {
-                    syslog (P_ERR, "Cannot allocate memory for entity URLs");
-                    goto over;
-                }
-
-                for (i = 0; i < _twt->entities->url_len; i++) {
-                    _twt->entities->urls[i].url = NULL;
-                    _twt->entities->urls[i].expanded_url = NULL;
-                    _twt->entities->urls[i].display_url = NULL;
-
-                    if (!(tweet_url = json_object_array_get_idx(tweet_urls, i))) {
-                        syslog (P_ERR, "Cannot retrieve url number %d from array", i);
-                        goto over;
-                    } else {
-                        if (!(_twt->entities->urls[i].url = string_from_object (tweet_url, "url"))) {
-                            syslog (P_ERR, "Cannot retrieve url number %d element (url)", i);
-                            goto over;
-                        }
-                        if (!(_twt->entities->urls[i].expanded_url = string_from_object (tweet_url, "expanded_url"))) {
-                            syslog (P_ERR, "Cannot retrieve url number %d element (expanded_url)", i);
-                            goto over;
-                        }
-                        if (!(_twt->entities->urls[i].display_url = string_from_object (tweet_url, "display_url"))) {
-                            syslog (P_ERR, "Cannot retrieve url number %d element (display_url)", i);
-                            goto over;
-                        }
-                    }
-                }
-            }
-
-            if ((tweet_medias = json_object_object_get (tweet_entities, "media"))) {
-                _twt->entities->media_len = json_object_array_length(tweet_medias);
-
-                if (!(_twt->entities->media = malloc (sizeof(struct Media) * _twt->entities->media_len))) {
-                    syslog (P_ERR, "Cannot allocate memory for entity URLs");
-                    goto over;
-                }
-
-                for (i = 0; i < _twt->entities->media_len; i++) {
-                    _twt->entities->media[i].url = NULL;
-                    _twt->entities->media[i].expanded_url = NULL;
-                    _twt->entities->media[i].display_url = NULL;
-                    _twt->entities->media[i].media_url = NULL;
-                    _twt->entities->media[i].type = NULL;
-
-                    if (!(tweet_media = json_object_array_get_idx(tweet_medias, i))) {
-                        syslog (P_ERR, "Cannot retrieve media number %d from array", i);
-                        goto over;
-                    } else {
-                        if (!(_twt->entities->media[i].type = string_from_object (tweet_media, "type"))) {
-                            syslog (P_ERR, "Cannot retrieve media number %d element (type)", i);
-                            goto over;
-                        }
-                        if (!(_twt->entities->media[i].url = string_from_object (tweet_media, "url"))) {
-                            syslog (P_ERR, "Cannot retrieve media number %d element (url)", i);
-                            goto over;
-                        }
-                        if (!(_twt->entities->media[i].expanded_url = string_from_object (tweet_media, "expanded_url"))) {
-                            syslog (P_ERR, "Cannot retrieve media number %d element (expanded_url)", i);
-                            goto over;
-                        }
-                        if (!(_twt->entities->media[i].display_url = string_from_object (tweet_media, "display_url"))) {
-                            syslog (P_ERR, "Cannot retrieve media number %d element (display_url)", i);
-                            goto over;
-                        }
-                        if (!(_twt->entities->media[i].media_url = string_from_object (tweet_media, "media_url"))) {
-                            syslog (P_ERR, "Cannot retrieve media number %d element (media_url)", i);
-                            goto over;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    twt = _twt;
-over:
-    if (_twt && (twt == NULL)) {
-        free_tweet (_twt);
-        _twt = NULL;
-    }
-
-    return twt;
-}*/
 
 int tweet_create_from_json (char *tweet_json_string) {
     int rc = -1;
@@ -314,8 +44,12 @@ int tweet_create_from_json (char *tweet_json_string) {
             key = (char *) entry->k;
             value = (json_object *) entry->v;
 
-            if(!(strncmp(key, "id_str", 6)) || !(strncmp(key, "text", 4)) || !(strncmp(key, "retweet_count", 13))) {
-                printf ("%s: %s\n", key, json_object_get_string (value));
+            switch (tokenize(key)) {
+                case TWTELM_ID_STR:
+                case TWTELM_TEXT:
+                case TWTELM_RETWEET_COUNT:
+                    printf ("%s: %s\n", key, json_object_get_string (value));
+                    break;
             }
         }
     }
@@ -327,37 +61,3 @@ over:
 
     return rc;
 }
-
-/*int tweet_create_from_json (char *tweet_json_string) {
-    int rc = -1;
-    json_object *tweet = NULL;
-    Tweet_p twt = NULL;
-
-    if ((tweet = json_tokener_parse(tweet_json_string)) == NULL) {
-        syslog(P_DBG, "Cannot parse tweet json");
-        syslog(P_DBG, "%s", tweet_json_string);
-        goto over;
-    }
-
-    if ((twt = tweet_create_from_object(tweet)) == NULL) {
-        syslog(P_DBG, "Cannot turn json object into tweet");
-        goto over;
-    }
-
-    // Worker code - will store tweet id's in database in production code
-
-    printf("User: @%s\n", twt->user->screen_name);
-    printf("Tweet text: %s\n", twt->text);
-
-    rc = 0;
-over:
-    if (twt) {
-        free_tweet (twt);
-        twt = NULL;
-    }
-
-    if (tweet)
-        json_object_put(tweet);
-
-    return rc;
-}*/
--- json.h
+++ json.h
@@ -3,50 +3,33 @@
 #ifndef _FT_JSON_H_
 #define _FT_JSON_H_
 
-typedef struct Url {
-    char *url;
-    char *expanded_url;
-    char *display_url;
-} *Url_p;
+enum tweet_element {
+    TWTELM_PLACE,
+    TWTELM_GEO,
+    TWTELM_RETWEETED,
+    TWTELM_TEXT,
+    TWTELM_IN_REPLY_TO_STATUS_ID_STR,
+    TWTELM_IN_REPLY_TO_STATUS_ID,
+    TWTELM_IN_REPLY_TO_USER_ID_STR,
+    TWTELM_TRUNCATED,
+    TWTELM_POSSIBLY_SENSITIVE_EDITABLE,
+    TWTELM_SOURCE,
+    TWTELM_IN_REPLY_TO_USER_ID,
+    TWTELM_CONTRIBUTORS,
+    TWTELM_RETWEET_COUNT,
+    TWTELM_FAVORITED,
+    TWTELM_CREATED_AT,
+    TWTELM_COORDINATES,
+    TWTELM_POSSIBLY_SENSITIVE,
+    TWTELM_ID_STR,
+    TWTELM_USER,
+    TWTELM_IN_REPLY_TO_SCREEN_NAME,
+    TWTELM_ID,
+    TWTELM_ENTITIES,
+    TWTELM_UNDEF
+};
 
-typedef struct Media {
-    char *type;
-    char *url;
-    char *media_url;
-    char *expanded_url;
-    char *display_url;
-} *Media_p;
-
-typedef struct Entities {
-    Url_p urls;
-    size_t url_len;
-    Media_p media;
-    size_t media_len;
-} *Entities_p;
-
-typedef struct User {
-    char *id_str;
-    char *screen_name;
-    char *name;
-    char *profile_image_url;
-    char *description;
-} *User_p;
-
-typedef struct Tweet {
-    char *id_str;
-    char *created_at;
-    char *text;
-    User_p user;
-    Entities_p entities;
-    void *retweeted_status;
-    int32_t retweet_count;
-    char *error;
-} *Tweet_p;
-
-void free_tweet(Tweet_p twt);
-int32_t int_from_string_object(json_object *object, const char *key);
-char *string_from_object(json_object *object, const char *key);
+enum tweet_element tokenize (char *key);
 int tweet_create_from_json(char *tweet);
-char send_email(const char *twitter_id, const char *media_url, const char *photo_url, const char *tweet_text);
 
 #endif
